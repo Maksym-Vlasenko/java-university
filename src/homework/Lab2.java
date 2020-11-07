efc605a6215ca92d81c4ba34f8631058b626e940
Maksym Vlasenko <191160@nuos.edu.ua>
2020-11-07 22:30:28 +0300

    Deleted comment

--- src/homework/Lab2.java
+++ src/homework/Lab2.java
@@ -32,7 +32,6 @@ public class Lab2 {
         System.out.printf("Average Y = %.3f%n", averageY);
     }
 
-    // This is a new comment
     public double calculateY(double x) {
         double y;
         double a = 2.4;
